5ecc849ff444ab41033c2a5b85ebc9830b0df79c
Oscar1707 <kodak17@MacBook-Air-de-Oscar.local>
2024-03-16 23:02:27 -0600

    no change

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
-FROM alpine:latest  
+FROM python:3.13.0a4-slim-bullseye
 WORKDIR /app
-RUN apk update && apk add --no-cache python3
 COPY . .
+RUN pip install --no-cache-dir -r requirements.txt
 CMD [ "python", "app.py"]
--- app.py
+++ app.py
@@ -1,8 +1,30 @@
+from bs4 import BeautifulSoup
 import time
 
-def cleanData():
-    print("Cleaning data...")
-    print("luni22")
-    return "exit"
+def bs4Scrap():
+    html_doc = """
+    <html><head><title>The Dormouse's story</title></head>
+    <body>
+    <p class="title"><b>The Dormouse's story</b></p>
+    
+    <p class="story">Once upon a time there were three little sisters; and their names were
+    <a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+    <a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+    <a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+    and they lived at the bottom of a well.</p>
+    
+    <p class="story">...</p>
+    """
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    print(soup.prettify())
+    demon4()
 
-cleanData()
+def demon4():
+    print("hello")
+
+    condition = False
+    while condition == False:
+        time.sleep(5)
+        print("waiting...")
+
+bs4Scrap()
