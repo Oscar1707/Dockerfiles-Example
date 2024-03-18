7d0230f10bdcc953423a7bce7ebb075c84955de9
Oscar Acacio <aao_07@hotmail.com>
2024-03-17 20:39:02 -0600

    fix/add pymon

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-import time
+import requests
 
-bole = False
 
-print("vamos a la playa")
-print("goku eta vaina e seria")
-while bole == False:
-    print("still live")
-    time.sleep(10)
+def requestParams():
+ 
+ r = requests.get('https://api.github.com/events')
+ print(r.json    )
+ return "hello"
+
+requestParams()
